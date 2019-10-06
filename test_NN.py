c894f840f84e16abadde2dbd1a53bd254a9607e4
wanga26 <alex.wang@emc.com>
2019-10-06 17:44:00 +0800

    Finish recoding NN framework from scratch

--- test_NN.py
+++ test_NN.py
@@ -1,4 +1,6 @@
 import numpy as np
+from sklearn.utils import resample
+import matplotlib.pyplot as plt
 
 class Node:
     """
@@ -38,12 +40,17 @@ class Node:
         """
         raise NotImplemented
 
+    def __repr__(self):
+        return "Node: {}".format(self.name)
+
 
 # define Input class
 class Input(Node):
     # there is no inputs for Input Nodes
-    def __init__(self, name, ini_value):
+    def __init__(self, name):
         Node.__init__(self, name, inputs=[])  # no inputs for Input Nodes
+
+    def get_init_value(self, ini_value):
         self.ini_value = ini_value
 
     def forward(self):
@@ -76,13 +83,10 @@ class Linear(Node):
         for output_node in self.outputs:
             grad_cost += output_node.gradients[self]
 
-        # TODO: here i want to check how many outupt nodes
-        # need to think twice    ??
-        # for matrix: D = W * X => dW = dD * X.T, dX = W.T * dD
-        # for matrix: Y = W * X => dW = dY * X.T, dX = W.T * dY
-        # ToDO: check the gradient formula, first need to check their dimension or shape
+        # for matrix: Y = X * W + b => dW = X.T * dY, dX = dY * W.T, db = np.sum(dY, axis=0, keepdims=False)
+        self.gradients[self.w_node] = np.dot(self.x_node.value.T, grad_cost)
         self.gradients[self.x_node] = np.dot(grad_cost, self.w_node.value.T)
-        self.gradients[self.w_node] = np.dot(grad_cost, self.w_node.value.T)
+        self.gradients[self.b_node] = np.sum(grad_cost, axis=0, keepdims=False)
 
 
 # define Sigmoid class
@@ -112,7 +116,6 @@ class MSE(Node):
         self.y_hat = y_hat
 
     def forward(self):
-        #TODO Here i want to check the shape of y_true and y_hat
         y_true_flatten = self.y_true.value.reshape(-1, 1)
         y_hat_flatten = self.y_hat.value.reshape(-1, 1)
         self.y_diff = y_true_flatten - y_hat_flatten
@@ -141,4 +144,161 @@ def train_one_batch(topological_sorted_graph):
 def topological_sort(feed_dict):
     # starting with input nodes, since input nodes are in feed_dict
     input_nodes = [node for node in feed_dict.keys()]
+
+    starting_nodes = input_nodes[:]   # or     starting_nodes = list(input_nodes)
+
+    G = {}
+    while starting_nodes:
+        node = starting_nodes.pop(0)
+        if node not in G:
+            G[node] = {"in": set(), "out": set()}
+
+        for output_node in node.outputs:
+            if output_node not in G:
+                G[output_node] = {"in": set(), "out": set()}
+
+            G[node]['out'].add(output_node)
+            G[output_node]['in'].add(node)
+            starting_nodes.append(output_node)
+
+    # G = {}
+    # # traverse all nodes in G
+    # from_input_nodes = list(input_nodes)
+    # all_nodes = list(input_nodes)
+    # while from_input_nodes:
+    #     node = from_input_nodes.pop(0)
+    #     for output_node in node.outputs:
+    #         if output_node not in from_input_nodes:
+    #             from_input_nodes.append(output_node)
+    #
+    #         if output_node not in all_nodes:
+    #             all_nodes.append(output_node)
+    # # Filling G
+    # for any_node in all_nodes:
+    #     G[any_node] = {"in": any_node.inputs,
+    #                    "out": any_node.outputs}
+
+    L = []
+    # starting with input nodes, S records nodes whose inputs are None
+    S = set(input_nodes)
+    while S:
+        node = S.pop()
+
+        # Special handling input Nodes, need to feed the input value to input Nodes
+        # if isinstance(node, Input):
+        #     node.value = feed_dict[node]
+
+        L.append(node)
+        for output_node in node.outputs:
+            G[output_node]["in"].remove(node)
+            # not sure if the below line is necessary
+            G[node]["out"].remove(output_node)
+
+            if len(G[output_node]["in"]) == 0:
+                S.add(output_node)
+
+    # sorted topological node list
+    return L
+
+
+def sgd_update(trainable_nodes, learning_rate=1e-2):
+    for node in trainable_nodes:
+        # Only input node has node.gradients per itself
+        node.value += -1 * learning_rate * node.gradients[node]
+
+
+"""
+Node connection in this graph =>(Sort topology) Get sorted topological graph
+=> Run train_one_batch to forward and backward, getting the gradients
+=> Run sgd_update to update the variable node values
+=> Repeat train_one_batch and sgd_update until the loss is small enough => break
+"""
+
+# (Input) -> Linear -> Sigmoid -> Linear -> MSE
+
+# 1. Build the Nodes in this graph
+X, y = Input(name='X'), Input(name='y')   # tensorflow -> placeholder
+W1, b1 = Input(name='W1'), Input(name='b1')
+W2, b2 = Input(name='W2'), Input(name='b2')
+
+# 2. Build Node connections in this graph
+linear_output = Linear("linear_node_1", X, W1, b1)
+sigmoid_output = Sigmoid("sigmoid_node", linear_output)
+y_hat = Linear("linear_node_2", sigmoid_output, W2, b2)
+loss = MSE("mse_node", y, y_hat)
+
+# 3. Feed the input with values and sort topological
+#  3.1 load X,y from sklearn
+from sklearn.datasets import load_boston
+data = load_boston()
+X_ = data['data']
+X_ = (X_ - np.mean(X_, axis=0)) / np.std(X_, axis=0)
+n_features = X_.shape[1]
+y_ = data['target']
+
+n_hidden = 10  # number of neurons in the first layer
+
+W1_ = np.random.randn(n_features, n_hidden)
+b1_ = np.zeros(n_hidden)
+W2_ = np.random.randn(n_hidden, 1)
+b2_ = np.zeros(1)
+
+print("X_shape: {}".format(X_.shape))
+print("y_shape: {}".format(y_.shape))
+
+#  feed into X,Y and W,b
+feed_dict ={
+    X: X_,
+    y: y_,
+    W1: W1_,
+    b1: b1_,
+    W2: W2_,
+    b2: b2_
+}
+graph = topological_sort(feed_dict)
+W1.get_init_value(W1_)
+b1.get_init_value(b1_)
+W2.get_init_value(W2_)
+b2.get_init_value(b2_)
+print(graph)
+
+
+# 3. Begin to forward and get the final loss; Then backward to update the parameters
+losses = []
+epochs = 5000
+learning_rate = 1e-3
+
+batch_size = 64
+steps_per_epoch = X_.shape[0] // batch_size
+
+for epoch in range(epochs):
+    epoch_loss = 0
+
+    for batch in range(steps_per_epoch):
+        X_batach, y_batch = resample(X_, y_, n_samples=batch_size)
+        X.get_init_value(X_batach)
+        y.get_init_value(y_batch)
+
+        # Run forward => get the loss; Then Run backward => get the gradients
+        train_one_batch(graph)
+        # update the parameters per gradients
+        sgd_update(trainable_nodes=[W1, b1, W2, b2], learning_rate=learning_rate)
+
+        # Record current loss
+        epoch_loss += loss.value
+
+    losses.append(epoch_loss/steps_per_epoch)
+
+    if epoch % 100 == 0:
+        print("Epoch: {}, loss={:.3f}".format(epoch, epoch_loss/steps_per_epoch))
+
+plt.plot(losses)
+plt.show()
+
+print("Optimal parameters:")
+print(W1.value)
+print(b1.value)
+print(W2.value)
+print(b2.value)
+
 
